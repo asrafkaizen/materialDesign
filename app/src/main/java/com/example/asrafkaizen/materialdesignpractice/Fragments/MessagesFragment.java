50077e8173f586118605e9976f87ac621c2764ff
asrafkaizen <asrafkaizen@gmail.com>
2017-05-23 14:07:04 +0800

    added onItemSelectedListener

--- app/src/main/java/com/example/asrafkaizen/materialdesignpractice/Fragments/MessagesFragment.java
+++ app/src/main/java/com/example/asrafkaizen/materialdesignpractice/Fragments/MessagesFragment.java
@@ -6,8 +6,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.example.asrafkaizen.materialdesignpractice.R;
 
@@ -19,6 +21,7 @@ public class MessagesFragment extends Fragment {
 
     ArrayAdapter<CharSequence> mArrayAdapter;
     Spinner mSpn;
+    TextView tv;
 
     public MessagesFragment(){
         // Required empty public constructor
@@ -41,6 +44,24 @@ public class MessagesFragment extends Fragment {
         mSpn = (Spinner)rootView.findViewById(R.id.spn1);
         mSpn.setAdapter(mArrayAdapter);
 
+        tv = (TextView)rootView.findViewById(R.id.label);
+
+        mSpn.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // TODO Auto-generated method stub
+
+            }
+        });
+
         // Inflate the layout for this fragment
         return rootView;
     }
@@ -48,6 +69,10 @@ public class MessagesFragment extends Fragment {
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
+    }
+
+    public void onItemSelected(){
+
     }
 
     @Override

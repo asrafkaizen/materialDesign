92e0dac4712a2c1a0dd9c89e90f4aafb4d0fd4a5
asrafkaizen <asrafkaizen@gmail.com>
2017-05-23 13:57:25 +0800

    spinner populated

--- app/src/main/java/com/example/asrafkaizen/materialdesignpractice/Fragments/MessagesFragment.java
+++ app/src/main/java/com/example/asrafkaizen/materialdesignpractice/Fragments/MessagesFragment.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.asrafkaizen.materialdesignpractice.R;
 
@@ -14,6 +16,9 @@ import com.example.asrafkaizen.materialdesignpractice.R;
  */
 
 public class MessagesFragment extends Fragment {
+
+    ArrayAdapter<CharSequence> mArrayAdapter;
+    Spinner mSpn;
 
     public MessagesFragment(){
         // Required empty public constructor
@@ -28,8 +33,13 @@ public class MessagesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         View rootView = inflater.inflate(R.layout.fragment_messages, container, false);
 
+        mArrayAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.utp_courses, R.layout.textview_item);
+        mArrayAdapter.setDropDownViewResource(R.layout.textview_item);
+        mSpn = (Spinner)rootView.findViewById(R.id.spn1);
+        mSpn.setAdapter(mArrayAdapter);
 
         // Inflate the layout for this fragment
         return rootView;
